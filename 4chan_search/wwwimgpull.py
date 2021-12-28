eb84970e2d2a3e1a0d0f309ee5438d1d4243bb4f
grothedev <grothedev@gmail.com>
2021-12-28 11:19:03 -0500

    fixed host update script and added ability to pull pdfs from website

--- 4chan_search/wwwimgpull.py
+++ 4chan_search/wwwimgpull.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import sys
+import time
 
 ###
 # get images from websites
@@ -28,6 +29,34 @@ def pullImgs(url):
     for img in html.find_all('img'):
         srcURL = img.get('src')
         result.append(srcURL)
+    return result
+
+#def pullPDFs(url):
+#    return pullPDFs(url, 0)
+
+def pullPDFs(url, depth=0, alreadycrawled=[]):
+    if depth > 5 or url == '' or url is None or url in alreadycrawled: 
+        return [] 
+    baseurl=url[0:url.find('/', 8)+1]
+    result = []
+    print(url)
+    resp = requests.get(url)
+    html = BeautifulSoup(resp.text, 'html.parser')
+    alreadycrawled.append(url)
+    for a in html.find_all('a'):
+        url = a.get('href')
+        if url is None or url == '' or url == '/':
+            continue
+        if baseurl not in url and 'http' in url[0:4]: #this means that the url is pointing to external site
+            continue
+        print('found ' + url)
+        if 'http' not in url:
+            url = baseurl+url
+        if url.find('.pdf')>0 and os.path.isfile(url):
+            result.append(url)
+        else:
+            time.sleep(5)
+            result = result + pullPDFs(url, depth+1, alreadycrawled)
     return result
 
 #if len(sys.argv) < 2:
